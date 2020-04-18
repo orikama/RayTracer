03cc749d409dd1447971e5297ae83d2fa2714ffe
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2020-04-18 22:25:19 +0500

    first book finished

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@
 
 #include <thread>
 #include <vector>
-
+#include <mutex>
 
 const int g_ImageWidth = 800;
 const int g_ImageHeight = 400;
@@ -28,6 +28,89 @@ const int g_MaxDepth = 50;
 
 const int g_NumThreads = 4;
 
+
+class safe_cout
+{
+public:
+    safe_cout()
+    {
+        pixels_ready = 0;
+    }
+
+    void update_thread(int thread_index)
+    {
+        std::lock_guard lock(m);
+
+        pixels_ready += 400 + thread_index * 10;
+
+        std::cout << '\r' << std::flush;
+        std::cout << pixels_ready << '/' << g_ImageHeight * g_ImageWidth;
+        std::cout.flush();
+    }
+
+private:
+    int pixels_ready;
+    std::mutex m;
+};
+
+safe_cout g_Info;
+
+
+rt::hittable_list random_scene()
+{
+    rt::hittable_list world;
+
+    world.add(std::make_shared<rt::sphere>(rt::vec3(0.0, -1000.0, 0.0),
+                                           1000,
+                                           std::make_shared<rt::lambertian>(rt::vec3(0.5, 0.5, 0.5))));
+
+    int i = 1;
+    for (int a = -11; a < 11; ++a) {
+        for (int b = -11; b < 11; ++b) {
+            auto choose_material = rt::s_random_gen();
+            rt::vec3 center(a + 0.9 * rt::s_random_gen(), 0.2, b + 0.9 * rt::s_random_gen());
+
+            if ((center - rt::vec3(4.0, 0.2, 0.0)).length() > 0.9) {
+                // diffuse
+                if (choose_material < 0.5) {
+                    auto albedo = rt::s_random_gen.random_vec3() * rt::s_random_gen.random_vec3();
+                    world.add(std::make_shared<rt::sphere>(center,
+                                                           0.2,
+                                                           std::make_shared<rt::lambertian>(albedo)));
+                }
+                // metal
+                else if (choose_material < 0.75) {
+                    auto albedo = rt::s_random_gen.random_vec3() / 2.0 + 0.5; // interval [0.5, 1.0]
+                    auto fuzz = rt::s_random_gen() / 2.0; // interval [0.0, 0.5]
+                    world.add(std::make_shared<rt::sphere>(center,
+                                                           0.2,
+                                                           std::make_shared<rt::metal>(albedo, fuzz)));
+                }
+                // glass
+                else {
+                    world.add(std::make_shared<rt::sphere>(center,
+                                                           0.2,
+                                                           std::make_shared<rt::dielectic>(1.5)));
+                }
+            }
+        }
+    }
+
+    world.add(std::make_shared<rt::sphere>(rt::vec3(0.0, 1.0, 0.0),
+                                           1.0,
+                                           std::make_shared<rt::dielectic>(1.5)));
+
+    world.add(std::make_shared<rt::sphere>(rt::vec3(-4.0, 1.0, 0.0),
+                                           1.0,
+                                           std::make_shared<rt::lambertian>(rt::vec3(0.4, 0.2, 0.1))));
+
+    world.add(std::make_shared<rt::sphere>(rt::vec3(4.0, 1.0, 0.0),
+                                           1.0,
+                                           std::make_shared<rt::metal>(rt::vec3(0.7, 0.6, 0.5),
+                                                                       0.0)));
+
+    return world;
+}
 
 rt::vec3d ray_color(const rt::ray& r, const rt::hittable& world, int depth)
 {
@@ -56,6 +139,7 @@ rt::vec3d ray_color(const rt::ray& r, const rt::hittable& world, int depth)
 // TODO: random generator is not thread safe
 void render(int shift, rt::vec3<uint8_t>* img, rt::hittable_list& world, rt::camera& cam)
 {
+    int index = 1;
     for (int j = g_ImageHeight - 1; j >= 0; --j) {
         //std::cout << "\rScanlines remaining: " << j << ' ' << std::flush;
 
@@ -74,6 +158,12 @@ void render(int shift, rt::vec3<uint8_t>* img, rt::hittable_list& world, rt::cam
 
             //img[index++] = rt::vector_sqrt(color) * 255.999;
             img[j * g_ImageWidth + i] = rt::vector_sqrt(color) * 255.999;
+
+            ++index;
+            if (index == 400 + shift * 10) {
+                index = 0;
+                g_Info.update_thread(shift);
+            }
         }
     }
 }
@@ -81,37 +171,23 @@ void render(int shift, rt::vec3<uint8_t>* img, rt::hittable_list& world, rt::cam
 
 int main()
 {
-    const auto look_from = rt::vec3(3.0, 3.0, 2.0);
-    const auto look_at = rt::vec3(0.0, 0.0, -1.0);
+    const auto look_from = rt::vec3(13.0, 2.0, 3.0);
+    const auto look_at = rt::vec3(0.0, 0.0, 0.0);
     const auto up = rt::vec3(0.0, 1.0, 0.0);
-    const auto dist_to_focus = (look_from - look_at).length();
-    const double aperture = 2.0;
+    const auto dist_to_focus = 10.0;
+    const double aperture = 0.1;
     const double aspect_ratio = double(g_ImageWidth) / g_ImageHeight;
     rt::camera cam(look_from, look_at, up,
                    20, aspect_ratio,
                    aperture, dist_to_focus);
 
-    rt::hittable_list world;
-    world.add(std::make_shared<rt::sphere>(rt::vec3(0.0, 0.0, -1.0),
-                                           0.5,
-                                           std::make_shared<rt::lambertian>(rt::vec3(0.1, 0.2, 0.5))));
-    world.add(std::make_shared<rt::sphere>(rt::vec3(0.0, -100.5, -1.0),
-                                           100,
-                                           std::make_shared<rt::lambertian>(rt::vec3(0.8, 0.8, 0.0))));
-
-    world.add(std::make_shared<rt::sphere>(rt::vec3(1.0, 0.0, -1.0),
-                                           0.5,
-                                           std::make_shared<rt::metal>(rt::vec3(0.8, 0.6, 0.2),
-                                                                       0.0)));
-    world.add(std::make_shared<rt::sphere>(rt::vec3(-1.0, 0.0, -1.0),
-                                           0.5,
-                                           std::make_shared<rt::dielectic>(1.5)));
-    world.add(std::make_shared<rt::sphere>(rt::vec3(-1.0, 0.0, -1.0),
-                                           -0.45,
-                                           std::make_shared<rt::dielectic>(1.5)));
-
+    auto world = random_scene();
 
     auto* img = new rt::vec3<uint8_t>[g_ImageHeight * g_ImageWidth];
+
+
+    std::cout << "Pixels: " << g_ImageHeight * g_ImageWidth << std::endl;
+
     std::vector<std::thread> threads;
 
     auto start_t = std::chrono::high_resolution_clock::now();
